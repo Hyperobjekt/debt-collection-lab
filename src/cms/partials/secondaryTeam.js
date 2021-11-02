1507b692a0a347921a17047fd0d8fee4d95d083c
alegzandach <alex.deutch@gmail.com>
2021-11-02 15:42:42 -0400

    add primary and secondary team objects, add relevant field configs for both

---
+++ src/cms/partials/secondaryTeam.js
@@ -0,0 +1,21 @@
+const secondaryTeam = {
+  label: "Secondary Team",
+  name: "secondaryTeam",
+  widget: "list",
+  fields: [
+    {
+      label: "Name",
+      name: "name",
+      widget: "string",
+      required: false,
+    },
+    {
+      label: "Role",
+      name: "role",
+      widget: "string",
+      required: false,
+    },
+  ],
+};
+
+export default secondaryTeam;
